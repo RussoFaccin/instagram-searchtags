faf3bbb6ec07cfae45eabc94bc65e98e0ee4040a
Rodrigo Russo <rodrigorusso@iphonedeeduardo.seculoxxi.com>
2018-03-19 09:34:48 -0300

    Add spinner to data loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   appTitle = 'Angular - Instagram';
   searchTag  = '';
+  hasSpinner = false;
   data = [];
+  ngOnInit() {}
   onSearch() {
+    this.hasSpinner = true;
     const apiUrl = `https://www.instagram.com/explore/tags/${this.searchTag}/?__a=1`;
     fetch(apiUrl).then(response => {
       response.json().then(data => {
         this.data = data.graphql.hashtag.edge_hashtag_to_media.edges;
+        this.hasSpinner = false;
       });
     });
   }
